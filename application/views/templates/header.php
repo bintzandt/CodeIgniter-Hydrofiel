9268376c664fc663067bb0de0180904a6e328573
Bram in 't Zandt <bramintzandt@Trinity.local>
2018-11-09 15:42:22 +0100

    :sparkles: Make it easier to setup a new environment

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -37,7 +37,7 @@
     <meta name="theme-color" content="#ffffff">
 
     <!--        css-->
-    <link rel="stylesheet" href="/css/hydrofiel.css"/>
+    <link rel="stylesheet" href="/assets/hydrofiel.css"/>
     <!--    fonts-->
     <link href='//fonts.googleapis.com/css?family=Open+Sans+Condensed:300,300italic,700' rel='stylesheet' type='text/css'>
     <link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
