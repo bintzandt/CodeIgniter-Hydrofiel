977bca93c46b8e4544e820b672367dc698869e86
Unknown <b.z@posteo.nl>
2018-11-14 19:53:54 +0100

    :bug: Moved facebook logic to a seperate file. Updated Gruntfile.

--- js/hydrofiel.js
+++ js/hydrofiel.js
@@ -10,28 +10,6 @@ function showAll(){
     $('.hide_all').toggleClass('hidden', false);
 }
 
-function getImage(id){
-    var url = "https://graph.facebook.com/";
-    var access_token = "/attachments?access_token=1787347374631170|MVXgVmEVfr8FjeOrCV_M-fP68Ys";
-    $.getJSON(url + id + access_token, function (json) {
-        if (json.data.length > 0) {
-            $.each(json.data, function (i, media){
-                if (media.media !== undefined) {
-                    $('#' + id).html("<img class='img-responsive no_margin' src='" + media.media.image.src + "'>");
-                }
-                else if (media.subattachments !== undefined) {
-                    $('#' + id).html("<img class='img-responsive no_margin' src='" + media.subattachments.data[0].media.image.src + "'>");
-                }
-            });
-        }
-        else {
-            //console.debug(id);
-            $('#' + id).remove();
-            $('#txt_' + id).toggleClass('col-md-9 col-sm-12');
-        }
-    });
-}
-
 function toggleInschrijf(val){
 	if (val==="1") {
 		$('#inschrijfdeadline').toggleClass('hidden', false);
@@ -83,32 +61,6 @@ jQuery(document).ready(function($) {
         window.location = $(this).data("href");
     });
 
-    if ($('#facebookfeed').length > 0) {
-        // URL to access
-        var url = "https://graph.facebook.com/hydrofiel/feed?access_token=1787347374631170|MVXgVmEVfr8FjeOrCV_M-fP68Ys&limit=7&fields=id,message&callback=?";
-        // JSON request to get the data
-        $.getJSON(url, function (json) {
-            var html = "";
-            //Add some formatting to the message
-            $.each(json.data, function (i, fb) {
-                if (fb.message != null) {
-                    html += "<div class='container container-item'>" +
-                        "<div class='col-md-3' id='" + fb.id + "'></div>" +
-                        "<div class='col-md-9' id='txt_" + fb.id + "' align='left'>" +
-                        "<p>" +
-                        $('<div>').html(fb.message).text() +
-                        "</p>" +
-                        "</div>" +
-                        "</div>" +
-                        "<hr>";
-                    getImage(fb.id);
-                }
-            });
-            //Remove the last <hr>
-            html = html.slice(0, -4);
-            $('#facebookfeed').html(html);
-        });
-    }
 
     $("h1").each(function(){
         $(this).addClass("oranje_tekst");
