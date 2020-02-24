c29bd1a56c627e75c5cfb5c2a54c06bce595a224
Bram <bintzandt@users.noreply.github.com>
2020-02-24 16:30:29 +0100

    Overwrite the CI_Email::send() function
    This way we can prevent CI from sending emails in the testing environment. This was a problem because Github Actions offered a way to send emails using the default mail command that were actually being send.
    
    In development, all mail is send to mailhog so the output from the email can be tested there.

---
+++ application/core/MY_Email.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Class MY_Email
+ *
+ * Overwrites the send class so it does not send any emails when testing.
+ */
+class MY_Email extends CI_Email {
+	public function __construct() {
+		parent::__construct();
+	}
+
+	/**
+	 * Overwrite the default send method, to prevent sending e-mails from the testing server.
+	 *
+	 * @param bool $auto_clear Whether to clear all variables after sending.
+	 *
+	 * @return bool true on success, false on failure.
+	 */
+	public function send( $auto_clear = true ) {
+		if ( ENVIRONMENT !== 'testing' ){
+			return parent::send( $auto_clear );
+		}
+
+		return true;
+	}
+}
